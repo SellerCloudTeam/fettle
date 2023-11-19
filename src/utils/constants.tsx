41f63729575348f54c9ec93d5fab2167f43ea214
Stanimir Stoyanov <stanimir@sellercloud.com>
2023-11-19 13:24:44 +0200

    vpn smoke check url, formatting

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -2,16 +2,19 @@ export const DAYS_BACK = 90;
 
 export const VPN_CHECK_MILLISECONDS = 3000;
 
-export const GITHUB_ORG_REPO = "SellerCloudTeam/fettle"
+export const GITHUB_ORG_REPO = "SellerCloudTeam/fettle";
 
-export const URL_VPN_CHECK = "https://health.sellercloud.com/health";
-export const URL_HEALTHCHECKS_UI_API = "https://health.sellercloud.com/health-api";
+export const URL_VPN_CHECK =
+  "https://health.sellercloud.com/health-api/ui-settings"; // Small JSON file for VPN smoke check
+
+export const URL_HEALTHCHECKS_UI_API =
+  "https://health.sellercloud.com/health-api"; // Health Checks UI API
 
 export const Status = {
-    OPERATIONAL: "Operational",
-    PARTIAL_OUTAGE: "Partial outage",
-    OUTAGE: "Outage",
-    UNKNOWN: "Unknown"
+  OPERATIONAL: "Operational",
+  PARTIAL_OUTAGE: "Partial outage",
+  OUTAGE: "Outage",
+  UNKNOWN: "Unknown",
 } as const;
 
-export type Statuses = typeof Status[keyof typeof Status];
+export type Statuses = (typeof Status)[keyof typeof Status];
